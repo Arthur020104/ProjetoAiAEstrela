04826155eab4176fb6fcc7d0384cce330c7bf99a
Arthur020104 <arthur020104@gmail.com>
2025-04-10 14:34:31 -0300

    modificacoes na dificuldade e bugs

--- Assets/Scripts/Scene1/Trail.cs
+++ Assets/Scripts/Scene1/Trail.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Trail : Mover
 {
     private bool _startedMoving = false;
+    [SerializeField]private float _timeToDestoyAftherReachTarget = 3.0f;
     
     protected override void Update()
     {
         base.Update();
         if(OnDestination() && _startedMoving)
         {
-            Destroy(this.gameObject, 1.0f);
+            Destroy(this.gameObject, _timeToDestoyAftherReachTarget);
         }
     }
     public override void GoTo(Vector2 destination)
